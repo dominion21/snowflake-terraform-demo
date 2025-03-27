8840ed715525ed4357c95e4766f906adbc844225
mike <kambj88@yahoo.com>
2025-03-27 12:49:49 -0400

    terraform demo updates

--- main.tf
+++ main.tf
@@ -18,7 +18,13 @@ terraform {
 provider "snowflake" {
 }
 
-resource "snowflake_database" "DEMO_Database" {
-  name    = "DEMO_Database"
-  comment = "Database for Snowflake Terraform demo"
+resource "snowflake_tbl" "orders" {
+  name    = "create or replace TABLE SPEEDY_ORDERS_RAW_STG (
+	ORDER_ID VARCHAR(16777216),
+	ORDER_DATETIME TIMESTAMP_NTZ(9),
+	ITEMS VARIANT,
+	SOURCE_FILE_NAME VARCHAR(16777216),
+	LOAD_TS TIMESTAMP_NTZ(9)
+);"
+  comment = "TABLE for Snowflake Terraform demo"
 }
