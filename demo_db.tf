dfcff590f5bf578b15ff8be440cfc0d3b331159d
mike <kambj88@yahoo.com>
2025-11-22 00:17:08 -0500

    updated table creation

--- demo_db.tf
+++ demo_db.tf
@@ -22,3 +22,25 @@ resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
+
+
+
+
+
+
+
+name: Example Workflow for Environment Files
+
+on: push
+
+jobs:
+  set_and_use_env_vars:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Set environment variable
+        run: echo "MY_ENV_VAR=myValue" >> $GITHUB_ENV
+
+      - name: Use environment variable
+        run: |
+          echo "The value of MY_ENV_VAR is $MY_ENV_VAR"
+
--- main.tf
+++ main.tf
@@ -1,30 +1,13 @@
-terraform {
-  required_providers {
-    snowflake = {
-      source  = "chanzuckerberg/snowflake"
-      version = "0.25.17"
-    }
-  }
-
-  backend "remote" {
-    organization = "dominionllc"
-
-    workspaces {
-      name = "gh-actions-demo"
-    }
-  }
-}
-
-provider "snowflake" {
-}
-
 resource "snowflake_tbl" "orders" {
-  name    = "create or replace TABLE SPEEDY_ORDERS_RAW_STG (
-	ORDER_ID VARCHAR(16777216),
-	ORDER_DATETIME TIMESTAMP_NTZ(9),
-	ITEMS VARIANT,
-	SOURCE_FILE_NAME VARCHAR(16777216),
-	LOAD_TS TIMESTAMP_NTZ(9)
-);"
+  name = <<EOT
+create or replace TABLE SPEEDY_ORDERS_RAW_STG (
+    ORDER_ID VARCHAR(16777216),
+    ORDER_DATETIME TIMESTAMP_NTZ(9),
+    ITEMS VARIANT,
+    SOURCE_FILE_NAME VARCHAR(16777216),
+    LOAD_TS TIMESTAMP_NTZ(9)
+);
+EOT
+
   comment = "TABLE for Snowflake Terraform demo"
 }
