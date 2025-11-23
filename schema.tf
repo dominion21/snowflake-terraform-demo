fc52bd7672d1096c9ac0e0d751b0cab52c65edc8
mike <kambj88@yahoo.com>
2025-11-23 11:20:37 -0500

    creating wh for terraform project

--- schema.tf
+++ schema.tf
@@ -3,3 +3,8 @@ resource "snowflake_schema" "demo_schema" {
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
 }
+
+resource "snowflake_warehouse" "terraform_wh"{
+  name = "terraform_wh"
+  comment =  "warehouse for terraform project"
+}
